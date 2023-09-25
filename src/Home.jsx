8704d37a34afb1ed976e7a88c8e2a987d7d235a0
Taiwo AKerele <davcodes24@gmail.com>
2023-09-25 20:08:53 +0100

    just learn routing

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'My new website', body: 'lorem ipsum ...', author: 'John', id:1},
-        {title: 'Love', body: 'lorem ipsum ...', author: 'Kelvin', id:2},
-        {title: 'Gospel', body: 'lorem ipsum ...', author: 'John', id:3}
+        {title: 'Love', body: 'lorem ipsum ...', author: 'Kelvin', id:2}
     ]);
+
+    const [name, setName] = useState('mario')
+
+    const handleDelete =(id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('use effect ran')
+        console.log(name);
+    },[name])
     return (
         <div className="home">
-            <BlogList blogs={blogs} />
+            <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
         </div>
       );
 }
